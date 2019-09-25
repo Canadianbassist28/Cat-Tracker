5f100fea019a30892a9eb601cb2a8d1aa5bd918a
Canadianbassist28 <Canadianbassist28@gmail.com>
2019-09-25 16:52:04 -0400

    Added directories for subsystems

--- RealSense.py
+++ RealSense.py
@@ -6,9 +6,7 @@ from timeit import default_timer as timer
 
 pipeline = rs.pipeline()
 config = rs.config()
-#config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-#config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-#config.enable_stream(rs.stream.gyro)
+
 config.enable_stream(rs.stream.accel)
 config.enable_stream(rs.stream.gyro)
 
