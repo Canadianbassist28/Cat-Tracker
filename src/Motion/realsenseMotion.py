4957d7607285f24db38ad543d4857ff964c047a8
Canadianbassist28 <Canadianbassist28@gmail.com>
2019-10-08 17:18:10 -0400

    +added integration function to motion class, +added initial functionallity to track angle

--- src/Motion/realsenseMotion.py
+++ src/Motion/realsenseMotion.py
@@ -1,19 +1,62 @@
 import pyrealsense2 as rs
+from timeit import default_timer as timer
 
 class realsenseMotion(object):
 	"""object to handle motion data from the realsense"""
 	def __init__(self):
-		self.accelData = None
-		self.gyroData = None
+		_timeNow = timer()
 
-	def get_motion_frames(self,frames):
+		self.accelData = rs.vector()
+		self.gyroData = rs.vector()
+		self.VelData = (0,0,0)
+
+		self.lastaccelData = rs.vector()
+		self.lastGyroData = rs.vector()
+		self.lastVelData = rs.vector()
+
+		self.lastVelTime = _timeNow
+		self.lastaccelTime = _timeNow
+		self.lastGyroTime = _timeNow
+
+		self.angle = (0,0,0) #roll, pitch, yaw
+		self.position = (0,0,0) #x,y,z
+
+	def get_data(self,frames):
+
+		timeNow = timer()
+
+		self.lastGyroData = self.gyroData
+		self.lastaccelData = self.accelData
+
 		for frame in frames:
 			profile = frame.get_profile()
 			if profile.stream_type() == rs.stream.accel and profile.format() == rs.format.motion_xyz32f:
 					self.accelData = frame.as_motion_frame().get_motion_data()
+					self.lastaccelTime = timeNow
+
 
 			if profile.stream_type() == rs.stream.gyro and profile.format() == rs.format.motion_xyz32f:   
 					self.gyroData = frame.as_motion_frame().get_motion_data()
+					self.lastGyroTime = timeNow
+
+		tmp = self.__integrate(self.gyroData, self.lastGyroData, self.lastGyroTime)
+		self.angle = tuple(map(sum, zip(self.angle, tmp)))
+
 	
-	def __repr__(self):
-		return ("Accelerometer: {}\n".format(self.accelData)) + ("Gyroscope: {}".format(self.gyroData))
+	def __integrate(self, data, lastData, lastTime):
+		""" Uses trapozoidal integration on a vector [x,y,z] 
+			@return a tuple (x,y,z)"""
+
+		width = timer() - lastTime
+
+		xrect = width * lastData.x
+		yrect = width * lastData.y
+		zrect = width * lastData.z
+
+		xtri = 0.5 * width * (data.x - lastData.x)
+		ytri = 0.5 * width * (data.y - lastData.y)
+		ztri = 0.5 * width * (data.z - lastData.z)
+
+		return ((xrect + xtri), (yrect + ytri), (zrect + ztri))
+
+	
--- src/Motion/translationTest.py
+++ src/Motion/translationTest.py
@@ -2,29 +2,39 @@
 import numpy as np
 import pyrealsense2 as rs
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
 from realsenseMotion import realsenseMotion
+from timeit import default_timer as timer
+
 
 pipeline = rs.pipeline()
 cfg = rs.config()
-cfg.enable_stream(rs.stream.accel)
-cfg.enable_stream(rs.stream.gyro)
-#cfg.enable_device_from_file("src/Motion/sample.bag")
+#cfg.enable_stream(rs.stream.accel)
+#cfg.enable_stream(rs.stream.gyro)
+cfg.enable_device_from_file("src/Motion/sample.bag")
 
 pipeline.start(cfg)
 
+yaw = []
+
 motion = realsenseMotion()
 try:
-	while 1:
-		#start = timer()
+	while len(yaw) < 10000:
+		start = timer()
 
 		frames = pipeline.wait_for_frames()
 
-		motion.get_motion_frames(frames)
+		motion.get_data(frames)
+		yaw.append(motion.angle[1])
 
-		print(motion)
-			
-		if (cv2.waitKey(1) & 0xFF == ord('q')):
-			cv2.destroyAllWindows()
-			break;
+		#print(1 / (timer() - start))
+			 
+		#if (cv2.waitKey(1) & 0xFF == ord('q')):
+		#	cv2.destroyAllWindows()
+		#	break;
 finally:
-    pipeline.stop()
+	pipeline.stop()
+	plt.plot(yaw)
+	plt.show()
