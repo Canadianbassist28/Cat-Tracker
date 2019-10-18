c6c349a413d10e7f3932c77fccdb85529b82b159
coco3526 <kevinkayak@hotmail.com>
2019-10-18 12:53:29 -0400

    Added member functiosn to retrive depth and color images and more.

--- src/Filters/Camera.py
+++ src/Filters/Camera.py
@@ -18,12 +18,12 @@ class realsenseBackbone:                                                        
     def setConfig(self):
         #sets the config setting for the cammera
         config = rs.config()
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        #config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)          #set res to 1280 720
+        #config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
         #config.enable_stream(rs.stream.accel)
         #config.enable_stream(rs.stream.gyro)
 
-        #config.enable_device_from_file("test.bag")    #can do ,false to not repeat
+        config.enable_device_from_file("test.bag")    #can do ,false to not repeat
         return config
 
     def getpipeline(self):
@@ -38,22 +38,37 @@ class realsenseBackbone:                                                        
         return frames
 
     def getDepthFrame(self, frame):
-        # input the frame thats collected and returns colorized image ad a array.
-        #return depth frames to be able to apply color map.
+        # input the frame thats collected
+        #return depth frames
         depthFrame = frame.get_depth_frame()
-        colorized = rs.colorizer()
-        colorized_depth = np.asanyarray(colorized.colorize(depthFrame).get_data())
-        return colorized_depth
+        return depthFrame
 
     def getColorFrame(self, frame):
-        # input the frame thats collected and returns color frame part.
+        # inputS the frame thats collected and returns color frame part.
          colorFrame = frame.get_color_frame()
+         return colorFrame
+
+    def colorImageCV2(self, frame):
+         #puts the color image into a numpy array for cv2 display.
+         colorFrame = self.getColorFrame(frame)
          colorImage = np.asanyarray(colorFrame.get_data())
          return colorImage
 
+    def depthImageCV2(self, frame):
+        #inputs the frame calls deoth frame to get depth frame.
+        #the apply color mappuing to the image into a numpyarray to be displayed in cv2
+        depthFrame = self.getDepthFrame(frame)
+        colorized = rs.colorizer(0)                                       #can change vaule for color map
+        colorized_depth = np.asanyarray(colorized.colorize(depthFrame).get_data())
+        return colorized_depth
 
+    def distancePixel(self,depthImage, x, y):
+        #send frame and get the frame
+        distance = depthImage.get_distance(x, y)
+        return distance
 
         #for loop in main to continuelsy get frames.
+
 
 
 profile = realsenseBackbone()
@@ -63,17 +78,11 @@ pipeline = profile.getpipeline()
 while True: #keeps going while it is reciving data
         
     frames = profile.getFrames() #get the frame from the camera 
-    depth_image = profile.getDepthFrame(frames)
-    color_image = profile.getColorFrame(frames)
-    #if not depth_image or not color_depth:
-      #  continue #to the top of the loop
+    depth_image = profile.depthImageCV2(frames)
+    color_image = profile.colorImageCV2(frames)
 
-    # Convert images to numpy arrays
-#    depth_image = np.asanyarray(depth_frame.get_data())
- #   color_image = np.asanyarray(color_frame.get_data())
-
-    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-  #  depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    #dist = profile.distance(0,0)
+    print(profile.distancePixel(frame ,0 ,0))
 
     # Stack both images horizontally see both images in same window
     images = np.hstack((color_image, depth_image))
