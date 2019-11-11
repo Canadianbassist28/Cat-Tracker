0cb935cfce96774ec4028c952360372828da1ced
coco3526 <kevinkayak@hotmail.com>
2019-11-11 14:25:17 -0500

    updated mapping file from the branch

--- src/mapping/mapping.py
+++ src/mapping/mapping.py
@@ -2,8 +2,9 @@ import pyrealsense2 as rs
 import math as math
 import numpy as np
 from timeit import default_timer as timer
-
-from src.motion.realsenseMotion import realsenseMotion as Motion
+import plotly.plotly as py
+import plotly.graph_objs as go
+from src.Motion.realsenseMotion import realsenseMotion as Motion
 
 
 class realsenseMap(object):
@@ -35,27 +36,74 @@ class realsenseMap(object):
     def load (self):
         #this function will take information from dylan's piece of the project and load it into a tuple to gain real time information.
         #this will be useful for mapping because the we can use it to map using this tuple
-        while(is rs.playing)
+        while(rs.playback_status.playing):
             Motion.self.position=(self.xaxis, self.yaxis, self.zaxis)
             self.data[position, angle]
 
-   def print(self.data)
+
+    def print(self):
    #this function will be able to print the map to a file
-    print self.xaxis
-    print self.yaxis
-    print self.zaxis
-    print self.axis
+        print(self.data)
 
-   def distance(self.data)
+    def distance(self):
    #this function will be able to calculate the distance from the initial point and create a tuple of distance from any point while the program is running 
-    distance= (math.sqrt((self.xaxis-init.self.xaxis)^2), math.sqrt((self.yaxis-init.self.yaxis)^2), math.sqrt((self.zaxis-init.self.zaxis)^2))
-    
-   def isTurn(self.data)
-    #this function will be able to decide if the camera has turned using the gyroscope
-    
-   def turnLeft(self.data)
-    #this function will identify a left turn
+        tot_distance= (math.sqrt((self.xaxis-init.self.xaxis)^2), math.sqrt((self.yaxis-init.self.yaxis)^2), math.sqrt((self.zaxis-init.self.zaxis)^2))
 
-   def turnRight(self.data)
+    def isTurn(self):
+    #this function will be able to decide if the camera has turned using the gyroscope
+        if turnLeft(self) == true or turnRight(self) == true:  
+             return True
+        else: 
+             return False
+    
+    def turnLeft(self):
+    #this function will identify a left turn
+         if self.xaxis > self.data.xaxis and self.angle > 270 and self.angle < 360:
+            return True
+         else: 
+            return False
+    def turnRight(self):
     #this function will display a right turn
+        if self.xaxis < self.data.xaxis and self.angle > 45 and self.angle < 90:
+            return True
+        else: 
+            return False
+# Creating the data
+def wireframe(self):
+    x = self.xaxis
+    y = self.yaxis
+    xGrid, yGrid = np.meshgrid(y, x)
+    R = np.sqrt(xGrid ** 2 + yGrid ** 2)
+    z = np.sin(R)
+    # Creating the plot
+    lines = []
+    line_marker = dict(color='#0066FF', width=2)
+    for i, j, k in zip(xGrid, yGrid, z):
+        lines.append(go.Scatter3d(x=i, y=j, z=k, mode='lines', line=line_marker))
+    layout = go.Layout(
+        title='Wireframe Plot',
+        scene=dict(
+            xaxis=dict(
+                gridcolor='rgb(255, 255, 255)',
+                zerolinecolor='rgb(255, 255, 255)',
+                showbackground=True,
+                backgroundcolor='rgb(230, 230,230)'
+            ),
+            yaxis=dict(
+                gridcolor='rgb(255, 255, 255)',
+                zerolinecolor='rgb(255, 255, 255)',
+                showbackground=True,
+                backgroundcolor='rgb(230, 230,230)'
+            ),
+            zaxis=dict(
+                gridcolor='rgb(255, 255, 255)',
+                zerolinecolor='rgb(255, 255, 255)',
+                showbackground=True,
+                backgroundcolor='rgb(230, 230,230)'
+            )
+        ),
+        showlegend=False,
+    )
+    fig = go.Figure(data=lines, layout=layout)
+    py.iplot(fig, filename='wireframe_plot')
         
